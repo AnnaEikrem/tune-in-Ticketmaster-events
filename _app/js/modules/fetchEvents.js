c6b4f63a3e65b3ed54488d003a2a0024f180ba04
Anna Eikrem <eikrem.anna@gmail.com>
2023-03-01 09:43:25 +0100

    Small adjustments to js file

--- _app/js/modules/fetchEvents.js
+++ _app/js/modules/fetchEvents.js
@@ -2,7 +2,8 @@ export default async function fetchEvents() {
 	const endpointEventsOslo = 'https://app.ticketmaster.com/discovery/v2/events?apikey=X8qgmCZMjDFoChlGd2mZXPbwpVSTvH9A&locale=*&size=200&city=Oslo&countryCode=NO&segmentName=Music';
 
 	const response = await fetch(endpointEventsOslo);
-	const results = await response.json();
+	const eventResults = await response.json();
 
-	console.log(results);
+	return eventResults;
+	
 }
