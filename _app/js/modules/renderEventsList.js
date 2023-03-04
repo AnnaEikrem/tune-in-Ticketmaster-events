6ec7aa45b2502f3006e932e2ec6b65f958707f84
Anna Eikrem <eikrem.anna@gmail.com>
2023-03-04 12:36:28 +0100

    Misc changes to function in renderEventsList

--- _app/js/modules/renderEventsList.js
+++ _app/js/modules/renderEventsList.js
@@ -7,11 +7,11 @@ export default function renderEventsList(events) {
 		});
 	}
 
+
 	function renderHTML(event) {
 		const eventImage = event.imageUrl;
 		const eventName = event.name;
 		const eventLocation = event.location;
-		// const eventCity = event.city;
 		const eventDate = event.date;
 		const eventTime = event.time;
 		const eventBuyTicket = event.buyLink;
@@ -19,13 +19,15 @@ export default function renderEventsList(events) {
 		const eventLocationPath = ("../assets/icons/Location-icon.png")
 		const buyLink = eventBuyTicket;
 	
+
 		const eventCard = document.createElement('div');
 		const eventImageElement = document.createElement('img');
 		const eventInformation = document.createElement('div');
 		const eventInformationLeft = document.createElement('div');
 		const eventInformationDate = document.createElement('div');
-		const eventInformationIcon = document.createElement('img');
 		const eventInformationTime = document.createElement('div');
+		const eventInformationIcon = document.createElement('img');
+		const eventTimeNumber = document.createElement('div');
 		const eventInformationCenter = document.createElement('div');
 		const eventCenterTitle = document.createElement('div');
 		const eventCenterLocation = document.createElement('div');
@@ -38,21 +40,22 @@ export default function renderEventsList(events) {
 	
 		eventImageElement.setAttribute('src', eventImage);
 		eventInformationDate.innerText = eventDate;
+		eventTimeNumber.innerText = eventTime;
 		eventInformationIcon.setAttribute('src', eventTimeIcon);
-		eventInformationTime.innerText = eventTime;
 		eventCenterTitle.innerText = eventName;
 		eventLocationIcon.setAttribute('src', eventLocationPath);
 		eventLocationPlace.innerText = eventLocation;
 		buyTicketmasterLink.innerText = 'Kjøp';
 		buyTicketmasterLink.setAttribute('href', buyLink)
-		
+
 		eventCard.classList.add('event__item--card');
 		eventImageElement.classList.add('event__item--picture');
 		eventInformation.classList.add('event__item--information');
 		eventInformationLeft.classList.add('event__information--left');
 		eventInformationDate.classList.add('event__left--date');
-		eventInformationIcon.classList.add('event__time--icon');
 		eventInformationTime.classList.add('event__left--time');
+		eventInformationIcon.classList.add('event__time--icon');
+		eventTimeNumber.classList.add('event__time--number');
 		eventInformationCenter.classList.add('event__information--center');
 		eventCenterTitle.classList.add('event__center--title');
 		eventCenterLocation.classList.add('event__center--location');
@@ -68,8 +71,9 @@ export default function renderEventsList(events) {
 		eventCard.appendChild(eventInformation);
 		eventInformation.appendChild(eventInformationLeft);
 		eventInformationLeft.appendChild(eventInformationDate);
-		eventInformationTime.appendChild(eventInformationIcon);
 		eventInformationLeft.appendChild(eventInformationTime);
+		eventInformationTime.appendChild(eventInformationIcon);
+		eventInformationTime.appendChild(eventTimeNumber);
 		eventInformation.appendChild(eventInformationCenter);
 		eventInformationCenter.appendChild(eventCenterTitle);
 		eventInformationCenter.appendChild(eventCenterLocation);
