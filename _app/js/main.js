6cb878b97826530d99af5252dfa9596a211069c4
Anna Eikrem <eikrem.anna@gmail.com>
2023-03-07 11:18:54 +0100

    Moved formatDate function to renderEventslist

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,6 @@
 import toggleNavigation from './modules/toggleNavigation.js';
 import fetchEvents from './modules/fetchEvents.js';
 import renderEventsList from './modules/renderEventsList.js';
-import formatDate from './modules/formatDate.js';
 import heroCarousel from './modules/heroCarousel.js';
 
 // Fetch data from Ticketmaster;
@@ -10,7 +9,6 @@ const fetchedData = await fetchEvents();
 
 toggleNavigation();
 renderEventsList(fetchedData);
-formatDate(fetchedData);
 heroCarousel(fetchedData);
 
 
