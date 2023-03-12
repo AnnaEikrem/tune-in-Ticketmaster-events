27231a90776d5ad0c3479ff434eec983ac833484
Anna Eikrem <eikrem.anna@gmail.com>
2023-03-12 14:01:29 +0100

    Added more data to fetch from API

--- _app/js/modules/fetchEvents.js
+++ _app/js/modules/fetchEvents.js
@@ -8,10 +8,13 @@ export default async function fetchEvents() {
 	const endpointEventsOslo = `https://app.ticketmaster.com/discovery/v2/events?apikey=${i}&locale=*&size=${eventSize}&city=${city}&countryCode=NO&segmentName=Music`;
 
 
-	// Legg til try/catch
 	const response = await fetch(endpointEventsOslo);
 	const eventResults = await response.json();
+	
+	// if (eventResults === response.ok) {
 
+	// }
+	// Legg til try/catch
 	const allData = eventResults._embedded.events.map(data => {
 		return {
 			name: data.name,
@@ -22,7 +25,9 @@ export default async function fetchEvents() {
 			location: data._embedded.venues[0].name,
 			imageUrl: data.images,
 			buyLink: data.url,
-			timeStamp: data.dates.start.dateTime
+			timeStamp: data.dates.start.dateTime,
+			subGenre: data.classifications[0].subGenre.name,
+			id: data.classifications[0].subGenre.id
 		}
 	});
 
