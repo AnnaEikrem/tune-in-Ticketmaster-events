1edb603ae756a13ed582270989e6aa6a35850ec0
Anna Eikrem <eikrem.anna@gmail.com>
2023-04-06 19:37:25 +0200

    Fixed renderButtonsHTML, now sorts and filters

--- _app/js/modules/filterButtons.js
+++ _app/js/modules/filterButtons.js
@@ -1,44 +1,24 @@
 export default function filterButtons(genreData) {
 	const buttonsContainer = document.querySelector('.genre__buttons--container');
-	const eventGenres = genreData.map(event => event.genre)
-
-	const uniqueGenres = [...new Set(eventGenres)]
+	const eventGenres = genreData
+		.filter ( function (data) {
+			return data.genre !== 'Undefined'
+		})
+		.map(event => event.genre);
+	const uniqueGenres = [...new Set(eventGenres)].sort();
 
 	if(buttonsContainer) {
-		for (let i = 0; i < uniqueGenres.length; i++)
 			renderButtonHTML();
 	}
+	console.log(eventGenres)
 
-	// console.log(genreData.genre)
 	function renderButtonHTML() {
-		const genreText = uniqueGenres;
-		const buttonElement = document.createElement('button');
-
-		buttonElement.classList.add('genre__button');
-
-		buttonElement.textContent = genreText
-
-		buttonsContainer.appendChild(buttonElement);
-		console.log(uniqueGenres)
+		uniqueGenres.forEach(genre => {
+			const buttonElement = document.createElement('button');
+			buttonElement.classList.add('genre__button');
+			buttonElement.textContent = genre;
+			buttonsContainer.appendChild(buttonElement);
+		})
 	}
-
-		// function renderButtonHTML(genre) {
-		// 	const genreName = genre.genre;
-		// 	const genreID = genre.genreID;
-		// 	const genreButton = document.createElement('button');
-
-		// 	genreButton.innerText = genreName;
-		// 	genreButton.classList.add('genre__button')
-		// 	genreButton.dataset.id = genreID;
-		// 	genreButton.addEventListener('click', () => {
-		// 		getEventsByGenre(genreID);
-		// 	});
-
-		// 	buttonsContainer.appendChild(genreButton);
-		// }
-
-		// function getEventsByGenre() {
-		// 	this.target.genreID
-		// }
 
 }
